6c81b4829de9a3728ab012d69d299d49c79eeb8e
hudheyfa <hudheyfa@gmail.com>
2022-09-28 05:23:17 +0300

    remove priority perperty

--- src/pages/login.js
+++ src/pages/login.js
@@ -42,7 +42,7 @@ function LoginPage() {
                 className="mx-auto h-12 w-auto"
                 src="/ishnaaz-Recovered.png"
                 alt="Workflow"
-                priority="true"
+
               />
               <h2 className="mt-6 text-center text-3xl tracking-tight font-bold text-gray-900">
                 Sign in to your account
